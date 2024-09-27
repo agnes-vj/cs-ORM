79fc3985b7d7f6de8ee2856325e500a7586b2d40
Rich Neat <151729655+nc-richard-neat@users.noreply.github.com>
2024-09-27 16:16:55 +0100

    Update Program.cs

--- ORMS/Program.cs
+++ ORMS/Program.cs
@@ -3,4 +3,4 @@
 var toys = Utils.DeserializeFromFile<List<Toy>>("./Resources/Toys.json");
 var dogs = Utils.DeserializeFromFile<List<Dog>>("./Resources/Dogs.json");
 //var parks = Utils.DeserializeFromFile<List<Park>>("./Resources/Parks.json");
-//var dogsParks = Utils.DeserializeFromFile<List<DogPark>>("./Resources/DogPark.json");
+//var dogParkVisits = Utils.DeserializeFromFile<List<DogPark>>("./Resources/DogPark.json");
